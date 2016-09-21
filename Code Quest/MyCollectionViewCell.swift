1e220f0f8aaa0f0ae89bf8ced33f285d2e1f0f41
(You) Narukami <younarukami@yous-imac.local>
2016-09-20 22:15:42 -0400

    Testing creation of UICollectionView cells generated by ViewController

---
+++ Code Quest/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  Code Quest
+//
+//  Created by (You) Narukami on 9/20/16.
+//  Copyright © 2016 Spookle. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myLabel: UILabel!
+}
